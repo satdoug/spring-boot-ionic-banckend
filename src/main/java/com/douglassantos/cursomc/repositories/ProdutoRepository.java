d0a476052e5d7e337f8fa90bc67c5c85312b9353
Douglas dos Santos <dm.santos2011@gmail.com>
2018-10-04 14:30:47 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/douglassantos/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.douglassantos.cursomc.repositories;
+
+import com.douglassantos.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
