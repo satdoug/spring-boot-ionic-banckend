dd9237676b77d5ccc50ce08ac6c5ab56ba71be77
Douglas dos Santos <dm.santos2011@gmail.com>
2018-10-08 19:38:19 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/douglassantos/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.douglassantos.cursomc.repositories;
+
+import com.douglassantos.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
