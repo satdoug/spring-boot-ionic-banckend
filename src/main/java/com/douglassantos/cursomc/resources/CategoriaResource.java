d350db1e9bc79fb02a7f55aab90774e89af81417
Douglas dos Santos <dm.santos2011@gmail.com>
2018-10-18 13:32:13 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/douglassantos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/douglassantos/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import com.douglassantos.cursomc.domain.Categoria;
 import com.douglassantos.cursomc.dto.CategoriaDTO;
 import com.douglassantos.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -51,6 +52,17 @@ public class CategoriaResource {
     public ResponseEntity<List<CategoriaDTO>> findAll(){
         List<Categoria> list = categoriaService.findAll();
         List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+        Page<Categoria> pageList = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDTO = pageList.map(obj -> new CategoriaDTO(obj));
         return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/douglassantos/cursomc/services/CategoriaService.java
+++ src/main/java/com/douglassantos/cursomc/services/CategoriaService.java
@@ -7,6 +7,9 @@ import com.douglassantos.cursomc.services.exceptions.DataIntegrityServiceExcepti
 import com.douglassantos.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -45,5 +48,10 @@ public class CategoriaService {
 
     public List<Categoria> findAll(){
         return categoriaRepository.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return categoriaRepository.findAll(pageRequest);
     }
 }
